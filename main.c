8055fc3a82c53e31ef364ace206b6f56d94468e0
Arkangit <lectus.est@gmail.com>
2023-06-22 22:34:53 +0300

    a.out bug

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 // Here we go
 #include <stdio.h>
+#include <stdlib.h>
 
-int main () {
-    int n = 15;
+int main () 
+{     
+    char mas[10][20];
+    int i;
 
-    printf("%d\n", n);
+    sprintf(mas[0], "###################");
+
+    sprintf(mas[9], "###################");
+
+    for(i=0; i<10; i++)
+        printf("%s\n", mas[i]);
+
+    return 0;
+
 }
