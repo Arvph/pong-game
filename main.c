720e8341b076356c04053439ef80975503f57b32
Arkangit <lectus.est@gmail.com>
2023-06-22 22:39:43 +0300

    1st day of work. Field is done

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 
 int main () 
 {     
-    char mas[10][20];
+    char mas[10][21];
     int i;
 
     sprintf(mas[0], "###################");
+    for(i=1; i<9; i++)
+ 
+        sprintf(mas[i], "#                 #");
 
     sprintf(mas[9], "###################");
 
     for(i=0; i<10; i++)
+
         printf("%s\n", mas[i]);
 
     return 0;
